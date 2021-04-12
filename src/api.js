8a005464a46440149b6008543c7b1eeabf393d27
Matthew Jensen <mjens02@gmail.com>
2021-04-12 10:34:26 -0500

    #2 add api key to exchange rate request

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
+import config from "./config.json";
+
 // helper to make our exchange rate API call
 export function getExchangeRates(base, supportedCurrencies) {
   const symbols = supportedCurrencies
     .filter((symbol) => symbol !== base) // exclude your own code from requested symbols
     .join();
-  const url = `https://api.exchangeratesapi.io/latest?base=${base}&symbols=${symbols}`;
+  const url = `http://api.exchangeratesapi.io/v1/latest?access_key=${config.apiKey}&symbols=${symbols}`;
   return fetch(url)
     .then((res) => res.json())
     .then((res) => res.rates);
